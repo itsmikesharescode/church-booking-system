d1d97f86453f9a42290c6c0b278a48ac66955f53
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-18 21:18:18 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import '@fontsource-variable/lora';
+
+	const { data, children } = $props();
+</script>
+
+<ModeWatcher />
+<Toaster />
+{@render children()}
+
+<style>
+	:global(html) {
+		font-family: 'Lora Variable', serif;
+	}
+</style>
