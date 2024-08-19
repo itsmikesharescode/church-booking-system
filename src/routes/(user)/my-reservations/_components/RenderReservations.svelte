b03265df2c91181671fa6ab2e7458af74aa1d466
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-19 17:26:30 +0800

    add reservation UI

--- src/routes/(user)/my-reservations/_components/RenderReservations.svelte
+++ src/routes/(user)/my-reservations/_components/RenderReservations.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+	import ViewReservation from './_operations/ViewReservation.svelte';
 </script>
 
-<div class="grid grid-cols-4">
-	<p>CLIENT NAME</p>
-	<p>REFERENCE NUMBER</p>
-	<p>STATUS</p>
+<div class="flex flex-col">
+	<div
+		class="sticky top-[4.8rem] hidden bg-secondary p-[1rem] text-sm font-semibold sm:grid sm:grid-cols-[50%,30%,20%] md:grid-cols-[40%,30%,15%,15%]"
+	>
+		<p>CLIENT NAME</p>
+		<p class="hidden md:block">REFERENCE NUMBER</p>
+		<p>STATUS</p>
+	</div>
+
+	<div class="">
+		{#each Array(20) as _}
+			<div
+				class="grid border-b-[1px] p-[1rem] text-sm sm:grid-cols-[50%,30%,20%] md:grid-cols-[40%,30%,15%,15%]"
+			>
+				<p class="flex items-center">Eviota, Mike John Baguinaon</p>
+				<p class="hidden items-center md:flex">WD-000002</p>
+				<p class="flex items-center">On-going</p>
+				<div class="flex items-center justify-end p-2">
+					<ViewReservation />
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
