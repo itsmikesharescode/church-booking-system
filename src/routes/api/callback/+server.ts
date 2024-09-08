e253e01047139fff39c7728709ebf6291199bb87
itsmikesharescode <mikejohneviota@gmail.com>
2024-09-08 20:16:01 +0800

    add server webhook

--- src/routes/api/callback/+server.ts
+++ src/routes/api/callback/+server.ts
@@ -4,14 +4,14 @@ export const POST: RequestHandler = async ({ locals: { supabaseAdmin }, request 
 	const body = await request.json();
 	console.log(body);
 	if (body.status === 'PAID') {
-		const { error: insertError } = await supabaseAdmin.from('orders_tb').insert([
-			{
-				user_id: body.external_id,
-				xendit_callback: body
-			}
-		]);
+		const { error } = await supabaseAdmin.rpc('xendit_cb', {
+			xendit_callback_c: body,
+			user_id_c: body.external_id.split('/')[0],
+			booking_id_c: body.external_id.split('/')[1],
+			church_id_c: body.external_id.split('/')[2]
+		});
 
-		if (insertError) console.log(insertError); // intentional error side effect for vercel logging
+		if (error) console.log(error);
 	}
 
 	return json({ status: 'success' });
