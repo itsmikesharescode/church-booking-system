cfb91600fda934b964aef91791c48b56ae9bfccf
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-27 20:41:13 +0800

    add tanstack core

---
+++ src/routes/(user)/my-reservations/_components/table/components/index.ts
@@ -0,0 +1,8 @@
+export { default as DataTableCheckbox } from './table-check-box.svelte';
+export { default as DataTableRowActions } from './table-row-action.svelte';
+export { default as DataTableColumnHeader } from './table-column-header.svelte';
+export { default as DataTableToolbar } from './table-toolbar.svelte';
+export { default as DataTablePagination } from './table-pagination.svelte';
+export { default as DataTableViewOptions } from './table-view-options.svelte';
+export { default as TableFullnameRow } from './table-fullname-row.svelte';
+export { default as TableInterestRow } from './TableInterestRow.svelte';
